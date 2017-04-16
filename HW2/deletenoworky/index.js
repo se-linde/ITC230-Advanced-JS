d0797ef8dbee650202912c664f052239c145f968
Sara Dwayne Linde <lindesara@gmail.com>
2017-04-15 21:34:01 -0700

    HW2 homework
    Call to Delete doesn’t work from the router.

---
+++ HW2/deletenoworky/index.js
@@ -0,0 +1,76 @@
+//bookswitch.js
+"use strict"; 
+
+var http = require ('http'); 
+var fs = require ('fs'); 
+var qs = require("querystring"), hams = require("./lib/callsigns"); 
+var qsnew = require("querystring"), deleteObject = require("./lib/callsigns");
+
+// This is the function that sets up the pathing and 
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200; 
+    fs.readFile(__dirname + path, function(err, data) {
+        if(err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain'}); 
+            res.end('500 - Internal Error of Doom'); 
+        } else {
+            res.writeHead(responseCode, 
+                {'Content-Type': contentType}); 
+            res.end(data);         
+        }
+        
+    }); 
+}
+
+// The switch in the app. 
+
+http.createServer(function(req, res) {
+    
+    
+    var url = req.url.split("?"); 
+    console.log(url)
+    
+    var params = qs.parse(url[1]); 
+    console.log(params); 
+    // Normalizing the url, making the extra trailing slash optional, 
+    // making the url lowercase. 
+    
+    var path = url[0].toLowerCase();
+    
+    // The switch!
+    
+    switch(path) {
+        case '/': 
+            res.writeHead (200, {'Content-Type': 'text/plain'});
+            res.end('Welcome to the Callsign App!');
+            break;
+        
+        case '/search':
+            console.log(hams.get(params.callsign))
+            res.writeHead (200, {'Content-Type': 'text/plain'});
+            res.end('Welcome to the Callsign App Search Page!'); 
+            break;    
+        case '/add':
+            console.log(hams.get(params.callsign))
+            res.writeHead (200, {'Content-Type': 'text/plain'});
+            res.end('Welcome to the callsign add page!'); 
+            break;    
+        case '/delete':
+            console.log(hams.get(params.callsign))
+            res.writeHead (200, {'Content-Type': 'text/plain'});
+            res.end('Welcome to the Callsign App Delete Page!'); 
+            res.send(deleteObject)
+            break;        
+        default:  
+            res.writeHead (404, {'Content-Type': 'text/plain'});
+            res.end('404: page not found'); 
+            break;
+    }
+    
+        
+        
+}).listen(3000); 
+
+console.log('Server started on 127.0.0.1:3000; press Ctrl+C to terminate....'); 
+
