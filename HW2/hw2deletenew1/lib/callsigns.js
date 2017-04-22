cd4cc0a5f0e84d8ee07fb08da3b1c68aa879317d
Sara Dwayne Linde <lindesara@gmail.com>
2017-04-22 15:46:08 -0700

    Final version of HW2
    .

--- HW2/hw2deletenew1/lib/callsigns.js
+++ HW2/hw2deletenew1/lib/callsigns.js
@@ -1,17 +1,17 @@
 "use strict"; // Need this to have let work here. 
 
 // This is my array of objects: 
-let hams = [{id: 1, callsign: "A1AAA", operator: "Bones Hillman", zip: 98108},
-                {id: 2, callsign: "A1AAB", operator: "Phil Judd", zip: 98101},
-                {id: 3, callsign: "A1AAC", operator: "Elizabeth Cook", zip: 53703},
-                {id: 4, callsign: "N7YYZ", operator: "Geddy Lee", zip: 53703},
-                {id: 5, callsign: "A1AAD", operator: "Michael Good", zip: 11010},
-                {id: 6, callsign: "A1AAE", operator: "Dolly Parton", zip: 65473},
-                {id: 7, callsign: "A1AAF", operator: "kd lang", zip: 12839},
-                {id: 8, callsign: "A1AAG", operator: "Rob Hirst", zip: 09824},
-                {id: 9, callsign: "A1AAH", operator: "Jim Moginie", zip: 98274},
-                {id: 10, callsign: "A1AAI", operator: "Martin Rotsey", zip: 29348},
-                {id: 11, callsign: "A1AAJ", operator: "Peter Garrett", zip: 29348},
+let hams = [{callsign: "A1AAA", operator: "Bones Hillman", zip: 98108},
+                {callsign: "A1AAB", operator: "Phil Judd", zip: 98101},
+                {callsign: "A1AAC", operator: "Elizabeth Cook", zip: 53703},
+                {callsign: "N7YYZ", operator: "Geddy Lee", zip: 53703},
+                {callsign: "A1AAD", operator: "Michael Good", zip: 11010},
+                {callsign: "A1AAE", operator: "Dolly Parton", zip: 65473},
+                {callsign: "A1AAF", operator: "kd lang", zip: 12839},
+                {callsign: "A1AAG", operator: "Rob Hirst", zip: 09824},
+                {callsign: "A1AAH", operator: "Jim Moginie", zip: 98274},
+                {callsign: "A1AAI", operator: "Martin Rotsey", zip: 29348},
+                {callsign: "A1AAJ", operator: "Peter Garrett", zip: 29348},
                ];
 
 
@@ -44,12 +44,6 @@ exports.get = (callsign) => {
 
 exports.add = (newHam) => {
     
-    var maxid = 0; 
-    
-    hams.map(function(obj) {
-        
-        if (obj.id > maxid) maxid = obj.id; 
-    }); 
     
     // var newID = id++; 
     // console.log(newID); 
@@ -57,9 +51,9 @@ exports.add = (newHam) => {
     var hamCallsign = newHam.callsign; 
     var hamOperator = newHam.operator; 
     var hamZip = newHam.zip; 
-    var hamid = maxid++; 
+     
     
-    var totalHam = {id : hamid, callsign : hamCallsign, operator : hamOperator, zip: hamZip}; 
+    var totalHam = {callsign : hamCallsign, operator : hamOperator, zip: hamZip}; 
     hams.push(totalHam);  
     
 }; 
