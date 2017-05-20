c1ef0c949ab098d5e5c844fb30423682a329cb2f
Sara Dwayne Linde <lindesara@gmail.com>
2017-05-19 20:43:47 -0700

    Commit of HW6.
    Just missing JSON, but all other elements are there.

--- HW6/index.js
+++ HW6/index.js
@@ -18,16 +18,66 @@ app.engine(".html", handlebars({extname: '.html', defaultLayout: 'main' }));
 app.set("view engine", ".html");
 
 
-// Get
+// Get - Home Page
 
 app.get('/', (req, res) => {
     Ham.find((err,hams) => {
         if (err) return next(err); 
         res.render('home', {hams: hams});
         
-    })
+    });
 });
 
+// Get - About Page. 
+
+app.get('/about', (req,res) => {
+    res.type('text/html'); 
+    res.render('about'); 
+}); 
+
+// Get - Details Page. 
+
+app.get('/get', (req,res) => {
+   Ham.findOne ({ callsign:req.query.callsign }, (err, ham) => {
+       if (err) return next(err); 
+       res.type('text/html');
+       res.render('details', {result: ham}); 
+   });  
+});
+
+// Post - Details Page. 
+
+app.post('/get', (req,res) => {
+   Ham.findOne ({ callsign:req.query.callsign }, (err, ham) => {
+       if (err) return next(err); 
+       res.type('text/html');
+       res.render('details', {result: ham}); 
+   });  
+});
+
+// Get - Delete Page
+
+app.get('/delete', (req,res) => {
+   Ham.remove ({callsign: req.query.callsign }, (err, result) => {
+        if (err) return next (err); 
+       let deleted = result.result.n !== 0; 
+       Ham.count((err, total) => {
+            res.type('text/html'); 
+            res.render('delete', {callsign: req.query.callsign, deleted: result.result.n !== 0, total: total} );
+       });
+   }); 
+});
+
+
+// 404 
+
+app.use((req,res) => {
+    res.type('text/plain');
+    res.status(404); 
+    res.send('404 - Page Not Found'); 
+}); 
+
+// Listens for signals from beyond.
 
 app.listen(app.get('port'), () => {
     console.log('Express started, hit Ctrl+C to end.');    
